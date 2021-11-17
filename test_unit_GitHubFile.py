f7c48a84336266f80bbd7e100f33e9475f3f473f
AaronReadman <readmana@tcd.ie>
2021-11-17 18:17:55 +0000

    Empty unit tests I expect to have

---
+++ test_unit_GitHubFile.py
@@ -0,0 +1,50 @@
+import unittest
+import scriptGather5TestNodes4
+import json
+import pymongo
+
+
+class TestingLCA(unittest.TestCase):
+   
+
+ #Test if the entire thing works once it has ran    
+    def test_BuoildDataObject(self):
+        assert(False)
+
+#Testing if categories given are correct. Do this by evaluating the size of the categories from a known address
+    def test_Cateogories(self):
+        assert(False)
+
+#Testing if repos given are correct. Do this by evaluating the size of the repos from a known address
+    def test_Repos(self):
+        assert(False)
+
+#Testing if the id returned is correct to ensure the correct amount of Nodes were added to the nodes/links array
+    def test_addNodes(self):
+        assert(False)
+    
+#Test if the entire thing works once it has ran    
+    def test_runWholeThingWithoutErrors(self):
+        assert(False)
+
+
+#Test if the entire thing works once it has ran    
+    def test_runWholeThingWithErrors(self):
+        assert(False)
+
+ 
+#Test if the entire thing works once it has ran    
+    def test_DataReceived(self):
+        assert(False)       
+
+
+
+        
+        
+
+ 
+ 
+
+
+if __name__=='__main__':
+    unittest.main()        
