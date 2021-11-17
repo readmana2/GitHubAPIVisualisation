1829971497d155d1826682dcf240e103d2f828a8
AaronReadman <readmana@tcd.ie>
2021-11-17 21:29:20 +0000

    Some tests I expect to run

--- test_unit_GitHubFile.py
+++ test_unit_GitHubFile.py
@@ -7,14 +7,29 @@ import pymongo
 class TestingGithubVisualisation(unittest.TestCase):
    
 
- #Test if the entire thing works once it has ran    
+ #Test if the entire thing works while building.   
     def test_BuoildDataObject(self):
-        assert(False)
+
+        GithubWorkObject=Githubclass("token","username")#
+        if GithubWorkObject.errorCode == -1
+            assert(False)
+
+        assert(True)
 
 #Testing if categories given are correct. Do this by evaluating the size of the categories from a known address
+#According to the Visualisation library I looked at and some basic messing around with tutorials,I will need categories which should correspond to repos
     def test_Cateogories(self):
-        assert(False)
 
+        # ###################################
+        #Creating the github Object with the specific Token  and username
+        GithubWorkObject=Githubclass("token","username")#
+        ##################################
+        # # Create the required Categories
+        createCategoriesArray(GithubWorkObject.categories,GithubWorkObject.repos)#Github Object should have this stuff
+
+        size=len(GithubWorkObject.categories)#Using a user address with only two categories
+        self.assertEqual(size,2)
+        
 #Testing if repos given are correct. Do this by evaluating the size of the repos from a known address
     def test_Repos(self):
         assert(False)
