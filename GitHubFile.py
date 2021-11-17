5f4b4e45fc66f42148a37ed74376d2fe8bdeaa1b
AaronReadman <readmana@tcd.ie>
2021-11-17 22:51:26 +0000

    Add repo Node function created

--- GitHubFile.py
+++ GitHubFile.py
@@ -2,13 +2,18 @@ from github import Github
 
 #The node class
 class Githubclass:
-    stringTest=None
+    
     errorCode=None
+    stringTest=None
     g=None
     username=None
     user=None
     repos=None
+    dictContributorListAllRepos =[]#The nodes
+    linksStuff=[]#The links of the Node
     categories=[]
+    t=0#The id of the node
+    saveReposInOrder=[]#The Repos
 
 
     def __init__(self, token, userName):
@@ -31,6 +36,22 @@ class Githubclass:
 
         print("Is This Working?")
         self.stringTest="successfullyInstantiated"
+
+
+def addRepositoryNodes(t,dictContributorListAllRepos,saveReposInOrder,repos):
+    # #Get all the repos
+    for repo in repos:  
+    #Adding repo Nodes
+    #contributions=value
+
+        idStuff={'id':t,'name': repo.name,'login': repo.name,'value':200,'category':repo.name,"symbolSize": 50,"symbol": "diamond"}
+        
+        dictContributorListAllRepos.append(idStuff)
+        saveReposInOrder.append(repo.name)
+        t+=1
+
+#E.g First 12 have an id of 0-11
+    return t
 
 
 
